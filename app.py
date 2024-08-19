f03f81bbf524a23a7e989742ca252f90cbc5af7a
MiguelGmz7 <oscar.gomez5178@alumnos.udg.mx>
2024-08-19 16:16:25 -0600

    Ingresamos las primeras librerias
        1-.     Ingresamos las librerias
        2-.     Creamos los archivos .txt de las credenciales
        3-.     usamos open() para abrir estos archivos

---
+++ app.py
@@ -0,0 +1,8 @@
+from telegram import Update
+from telegram import Application, CommandHandler, MessageHandler, filters, ContextTypes
+# Obtenemos las credenciales
+with open('api_key.txt','r') as file:
+    api_key = file.read()
+
+with open('username.txt','r') as file:
+    username = file.read()
